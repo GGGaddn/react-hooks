939fba5a2ef9658ee19701008f1e0c0cf7c4c901
GGGaddn <68422101+GGGaddn@users.noreply.github.com>
2022-02-17 08:21:54 +0300

    react-hooks2
    react-hooks2

--- react-hooks2/src/App.js
+++ react-hooks2/src/App.js
@@ -1,11 +1,10 @@
 import React, {useState, useEffect}  from 'react'
 import TodoList from './TodoList'
+import {Context} from './context'
 
 export default function App()  {
   const [todos, setTodos] = useState([])
   const [todoTitle, setTodoTitle] = useState('')
-
-  const handleClick =() => console.log('click')
 
   useEffect(() => {
     const raw = localStorage.getItem('todos') || []
@@ -13,17 +12,13 @@ export default function App()  {
   }, [])
 
   useEffect(() => {
-    document.addEventListener('click', handleClick)
     localStorage.setItem('todos',JSON.stringify(todos))
-    return () => {
-      document.removeEventListener('click',handleClick)
-    }
   }, [todos])
 
   const addTodo = event => {
     if (event.key === 'Enter') {
       setTodos ([
-          ...todos,
+        ...todos,
         {
           id: Date.now(),
           title: todoTitle,
@@ -34,9 +29,26 @@ export default function App()  {
     }
   }
 
-    return (
-      <div className="container">
-        <h1>Todo app</h1>
+  const removeTodo = id => {
+    setTodos(todos.filter(todo => {
+      return todo.id !== id
+    }))
+  }
+
+  const toggleTodo = id => {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        todo.complete = !todo.complete
+      }
+      return todo
+    }))
+  }
+  return (
+      <Context.Provider value={{
+        toggleTodo, removeTodo
+      }}>
+        <div className="container">
+          <h1>Todo app</h1>
 
           <div className="input-field">
             <input
@@ -49,6 +61,7 @@ export default function App()  {
           </div>
 
           <TodoList todos={todos} />
-      </div>
-    );
+        </div>
+      </Context.Provider>
+  );
 }
